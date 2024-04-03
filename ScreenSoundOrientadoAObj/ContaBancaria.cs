fbd21540db6986ed2fd86303ce3e30bd308ad4c9
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-03 11:01:25 -0300

    Realizando desafio 1 - Conta bancaria

---
+++ ScreenSoundOrientadoAObj/ContaBancaria.cs
@@ -0,0 +1,16 @@
+﻿// Desafio 1
+class ContaBancaria
+{
+    public int numIndicador;
+    public string titular;
+    public double saldo;
+    public string senha;
+
+    public void ExibirInformacoesDaConta()
+    {
+        Console.WriteLine($"Número Indicador: {numIndicador}");
+        Console.WriteLine($"Titular: {titular}");
+        Console.WriteLine($"Saldo da conta: {saldo}");
+        Console.WriteLine($"Senha: {senha}");
+    }
+}
--- ScreenSoundOrientadoAObj/Program.cs
+++ ScreenSoundOrientadoAObj/Program.cs
@@ -1,14 +1,26 @@
-﻿Musica musica1 = new Musica();
-musica1.nome = "Wonderful Life";
-musica1.artista = "Bring Me The Horizon";
-musica1.duracao = 273;
-musica1.disponivel = true;
+﻿//Musica musica1 = new Musica();
+//musica1.nome = "Wonderful Life";
+//musica1.artista = "Bring Me The Horizon";
+//musica1.duracao = 273;
+//musica1.disponivel = true;
 
-Musica musica2 = new Musica();
-musica2.nome = "Metallica";
-musica2.artista = "Enter Sandman";
-musica2.duracao = 310;
-musica2.disponivel = false;
+//Musica musica2 = new Musica();
+//musica2.nome = "Metallica";
+//musica2.artista = "Enter Sandman";
+//musica2.duracao = 310;
+//musica2.disponivel = false;
 
-musica1.ExibirFichaTécnica();
-musica2.ExibirFichaTécnica();
+//musica1.ExibirFichaTécnica();
+//musica2.ExibirFichaTécnica();
+
+// Desafio 1
+ContaBancaria contaBancaria1 =  new ContaBancaria();
+contaBancaria1.numIndicador = 1;
+contaBancaria1.titular = "Guiler";
+contaBancaria1.saldo = 1200000.50;
+contaBancaria1.senha = "DarkCat!203";
+contaBancaria1.ExibirInformacoesDaConta();
+
+// Desafio 2
+
+    
