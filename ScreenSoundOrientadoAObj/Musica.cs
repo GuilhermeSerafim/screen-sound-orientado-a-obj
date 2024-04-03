89c89e5220b67846d22f2cdd132f7dd5cb7433ce
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-03 11:46:13 -0300

    Manipulando props

--- ScreenSoundOrientadoAObj/Musica.cs
+++ ScreenSoundOrientadoAObj/Musica.cs
@@ -5,11 +5,17 @@
     public string Artista { get; set; }
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
+    public string DescricaoResumida
+    {
+        get
+        {
+            return $"A música {Nome} pertence a banda {Artista}";
+        }
+    }
 
     public void ExibirFichaTécnica()
     {
-        Console.WriteLine($"Nome: {Nome}");
-        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine(DescricaoResumida);
         Console.WriteLine($"Duração: {Duracao}");
         if (Disponivel)
         {
