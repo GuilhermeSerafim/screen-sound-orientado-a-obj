d47beabe5c08f041e916ce392c10e5ba5a0a89aa
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-03 10:50:07 -0300

    Criando metodo para exibiçao da ficha tecnica da musica

--- ScreenSoundOrientadoAObj/Musica.cs
+++ ScreenSoundOrientadoAObj/Musica.cs
@@ -1,8 +1,23 @@
 ﻿class Musica
 {
+    // Variaveis
     public string nome;
     public string artista;
-    public string album;
     public int duracao;
     public bool disponivel;
+
+    // Método
+    public void ExibirFichaTécnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano");
+        } else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
 }
--- ScreenSoundOrientadoAObj/Program.cs
+++ ScreenSoundOrientadoAObj/Program.cs
@@ -1,12 +1,14 @@
 ﻿Musica musica1 = new Musica();
 musica1.nome = "Wonderful Life";
 musica1.artista = "Bring Me The Horizon";
+musica1.duracao = 273;
+musica1.disponivel = true;
 
 Musica musica2 = new Musica();
 musica2.nome = "Metallica";
 musica2.artista = "Enter Sandman";
+musica2.duracao = 310;
+musica2.disponivel = false;
 
-Console.WriteLine($"Nome: {musica1.nome}");
-Console.WriteLine($"Artista: {musica1.nome}");
-Console.WriteLine($"Nome: {musica2.nome}");
-Console.WriteLine($"Artista: {musica2.nome}");
+musica1.ExibirFichaTécnica();
+musica2.ExibirFichaTécnica();
