91d0d684e4059197afcbf85e3468135369ac2923
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-03 11:37:35 -0300

    Aplicando conceito de Properties e seguindo nomenclatura

--- ScreenSoundOrientadoAObj/Program.cs
+++ ScreenSoundOrientadoAObj/Program.cs
@@ -1,17 +1,17 @@
-﻿//Musica musica1 = new Musica();
-//musica1.nome = "Wonderful Life";
-//musica1.artista = "Bring Me The Horizon";
-//musica1.duracao = 273;
-//musica1.disponivel = true;
+﻿Musica musica1 = new Musica();
+musica1.Nome = "Wonderful Life";
+musica1.Artista = "Bring Me The Horizon";
+musica1.Duracao = 273;
+musica1.Disponivel = true;
 
-//Musica musica2 = new Musica();
-//musica2.nome = "Metallica";
-//musica2.artista = "Enter Sandman";
-//musica2.duracao = 310;
-//musica2.disponivel = false;
+Musica musica2 = new Musica();
+musica2.Nome = "Metallica";
+musica2.Artista = "Enter Sandman";
+musica2.Duracao = 310;
+musica2.Disponivel = false;
 
-//musica1.ExibirFichaTécnica();
-//musica2.ExibirFichaTécnica();
+musica1.ExibirFichaTécnica();
+musica2.ExibirFichaTécnica();
 
 // Desafio 1
 //ContaBancaria contaBancaria1 =  new ContaBancaria();
@@ -22,7 +22,7 @@
 //contaBancaria1.ExibirInformacoesDaConta();
 
 // Desafio 2
-Carro carro1 = new Carro();
-carro1.Acelerar();
-carro1.Buzinando();
-carro1.Freiar();
+//Carro carro1 = new Carro();
+//carro1.Acelerar();
+//carro1.Buzinando();
+//carro1.Freiar();
