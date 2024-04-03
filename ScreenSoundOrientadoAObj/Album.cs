e47b8319ea36fb408e7d4338f0b523f6c42731ce
GuilhermeSerafim <guilerstudies@gmail.com>
2024-04-03 14:13:08 -0300

    Criando lista de musicas, e exibindo o album de musicas e a duração do mesmo

---
+++ ScreenSoundOrientadoAObj/Album.cs
@@ -0,0 +1,21 @@
+﻿class Album
+{
+    private List<Musica> musicas = new List<Musica>();  
+    public string Nome { get; set; }
+    public int DuracaoTotal => musicas.Sum(musica => musica.Duracao); // Soma das músicas
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Exibindo músicas do album {Nome}:\n");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Música: {musica.Nome}");
+        }
+        Console.WriteLine($"Duração total em segundos do álbum {Nome}: {DuracaoTotal}s");
+    }
+}
--- ScreenSoundOrientadoAObj/Program.cs
+++ ScreenSoundOrientadoAObj/Program.cs
@@ -42,3 +42,20 @@
 //produto.Estoque = -1;
 //produto.Preco = -2;
 //produto.MostrarProduto();
+
+using System.Runtime.InteropServices.Marshalling;
+
+Album albumDoQueen = new Album();
+albumDoQueen.Nome = "A night at the opera";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Love of my life";
+musica1.Duracao = 213;
+
+Musica musica2 =  new Musica();
+musica2.Nome = "Bohemian Rhapsody";
+musica2.Duracao = 354;
+
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+albumDoQueen.ExibirMusicasDoAlbum();
